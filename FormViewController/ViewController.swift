151d09cc6ef6cf30df142639c47d9ab4972d522b
Nwabuoku, Oge (on475k) <on475k@att.com>
2018-05-11 17:03:20 -0400

    FormVC Phase 2

--- FormViewController/ViewController.swift
+++ FormViewController/ViewController.swift
@@ -12,33 +12,86 @@ struct HotSpot {
     var isEnabled: Bool = true
     var password: String = "Hello"
 }
+
+extension HotSpot {
+    var enabledSectionTitle : String? {
+        return isEnabled ? "Personal Hotspot" : nil
+    }
+}
+struct Sections {
+    var cells : [FormViewCell]
+    var footerTitle: String?
+}
+
+class FormViewCell: UITableViewCell {
+    var shouldHighlight = false
+    var didSelect : (() -> ())?
+}
 //First Commit
 class ViewController: UITableViewController {
-    
+    var sections: [Sections] = []
     var toggle = UISwitch()
     var state = HotSpot() {
         didSet {
             print(state)
-            UIView.setAnimationsEnabled(false)
-            tableView.beginUpdates()
-            //Grab a hold of the footer reference
-            let footer = tableView.footerView(forSection: 0)
-            footer?.textLabel?.text = tableView(tableView, titleForFooterInSection: 0)
-            footer?.setNeedsLayout()
-            
-            let cell = tableView.cellForRow(at: IndexPath(row: 0, section: 1))
-            cell?.detailTextLabel?.text = state.password
-            tableView.endUpdates()
-            UIView.setAnimationsEnabled(true)
+            sections[0].footerTitle = state.enabledSectionTitle
+            sections[1].cells[0].detailTextLabel?.text = state.password
+            reloadSectionFooters()
         }
+    }
+    func reloadSectionFooters(){
+        UIView.setAnimationsEnabled(false)
+        tableView.beginUpdates()
+        //Grab a hold of the footer reference
+        //for index in section.indices
+        for (index, _) in sections.enumerated() {
+            let footer = tableView.footerView(forSection: index)
+            footer?.textLabel?.text = tableView(tableView, titleForFooterInSection: index)
+            footer?.setNeedsLayout()
+        }
+        tableView.endUpdates()
+        UIView.setAnimationsEnabled(true)
     }
     
     init() {
         super.init(style: .grouped)
+        buildSections()
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+    }
+    
+    func buildSections() {
+        
+        let toggleCell = FormViewCell(style: .value1, reuseIdentifier: nil)
+        toggleCell.textLabel?.text = "Personal Hostspot"
+        toggleCell.contentView.addSubview(toggle)
+        toggle.isOn = state.isEnabled
+        toggle.translatesAutoresizingMaskIntoConstraints = false
+        toggle.addTarget(self, action: #selector (toggleChanged(_:)), for: .valueChanged)
+        toggleCell.contentView.addConstraints([
+            toggle.centerYAnchor.constraint(equalTo: toggleCell.contentView.centerYAnchor),
+            toggle.trailingAnchor.constraint(equalTo: toggleCell.contentView.layoutMarginsGuide.trailingAnchor)
+            ])
+        
+        let passwordCell = FormViewCell(style: .value1, reuseIdentifier: nil)
+        passwordCell.textLabel?.text =  "Password"
+        passwordCell.detailTextLabel?.text = state.password
+        passwordCell.accessoryType = .disclosureIndicator
+        passwordCell.shouldHighlight = true
+        
+        let passwordVC = PasswordViewController(password: state.password) {[unowned self] in
+            self.state.password = $0
+        }
+        passwordCell.didSelect = {[unowned self] in
+            self.navigationController?.pushViewController(passwordVC, animated: true)
+        }
+        
+        sections = [
+            Sections(cells: [toggleCell], footerTitle: state.enabledSectionTitle),
+            Sections(cells: [passwordCell], footerTitle: nil)
+        ]
     }
     
     override func viewDidLoad() {
@@ -48,49 +101,32 @@ class ViewController: UITableViewController {
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
+        return sections.count
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return sections[section].cells.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .value1, reuseIdentifier: nil)
-        if indexPath.section == 0 {
-            cell.textLabel?.text = "Personal Hostspot"
-            cell.contentView.addSubview(toggle)
-            toggle.isOn = state.isEnabled
-            toggle.translatesAutoresizingMaskIntoConstraints = false
-            toggle.addTarget(self, action: #selector (toggleChanged(_:)), for: .valueChanged)
-            cell.contentView.addConstraints([
-                toggle.centerYAnchor.constraint(equalTo: cell.contentView.centerYAnchor),
-                toggle.trailingAnchor.constraint(equalTo: cell.contentView.layoutMarginsGuide.trailingAnchor)
-                ])
-        } else if indexPath.section == 1 {
-            cell.textLabel?.text =  "Password"
-            cell.detailTextLabel?.text = state.password
-            cell.accessoryType = .disclosureIndicator
-        }
-        return cell
+        
+        //First we need to look at the right section in the section arrays
+        //Then we need to pickout the right cell
+        return cell(forRow: indexPath)
     }
     
     override func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
-        if section == 0 {
-            return state.isEnabled ? "Personal Hotspot" : nil
-        }
-        return nil
+        return sections[section].footerTitle
     }
     override func tableView(_ tableView: UITableView, shouldHighlightRowAt indexPath: IndexPath) -> Bool {
-        return indexPath.section != 0
+        return cell(forRow: indexPath).shouldHighlight
     }
+    func cell(forRow indexPath: IndexPath) -> FormViewCell {
+        return sections[indexPath.section].cells[indexPath.row]
+    }
+ 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if indexPath.section == 1 {
-            let passwordVC = PasswordViewController(password: state.password) {[unowned self] in
-                self.state.password = $0
-            }
-            navigationController?.pushViewController(passwordVC, animated: true)
-        }
+        cell(forRow: indexPath).didSelect?()
     }
     
     @objc func toggleChanged(_ sender : Any) {
@@ -104,7 +140,7 @@ class PasswordViewController : UITableViewController {
     let onChange : (String) -> ()
     
     init(password: String, onChange: @escaping (String) -> ()) {
-         self.onChange = onChange
+        self.onChange = onChange
         super.init(style: .grouped)
         textField.text = password
     }
